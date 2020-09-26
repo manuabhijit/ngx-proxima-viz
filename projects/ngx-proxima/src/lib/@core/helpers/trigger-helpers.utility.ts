1c1958e79ed23807a7e921016233df2331a23453
Abhijit <abhijit.srivastava@elucidata.io>
2020-09-26 21:34:45 +0530

    [Added] redraw Utility

--- projects/ngx-proxima/src/lib/@core/helpers/trigger-helpers.utility.ts
+++ projects/ngx-proxima/src/lib/@core/helpers/trigger-helpers.utility.ts
@@ -1,6 +1,7 @@
 export class TriggerHelperUtility {
   public downloadChartAsSvgCount: number = 0;
   public downloadChartAsPngCount: number = 0;
+  public redrawCount: number = 0;
 
   public saveGraphAsSvg(): void {
     this.downloadChartAsSvgCount++;
@@ -8,5 +9,9 @@ export class TriggerHelperUtility {
 
   public saveGraphAsPng(): void {
     this.downloadChartAsPngCount++;
+  }
+
+  public redrawChart(): void {
+    this.redrawCount++;
   }
 }
